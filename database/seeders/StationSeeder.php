718bb5197caefeefc7630c3df3a0f5c4c4c1497b
ayanakkabi1 <aynour200609@gmail.com>
2026-03-10 14:49:21 +0000

    feat(seeders): create StationSeeder for test data
    - Generate sample charging station records
    - Populate database with initial station data

---
+++ database/seeders/StationSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Station;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Station::create([
+            'name'=>
+        ])
+    }
+}
