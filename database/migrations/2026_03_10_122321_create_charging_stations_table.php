f348a50a728dc92898d1890f3affc7447f5aef01
ayanakkabi1 <aynour200609@gmail.com>
2026-03-10 14:48:59 +0000

    feat(database): create charging_stations table
    - Add geographic location (latitude, longitude)
    - Add connector_type and availability status
    - Separate table for detailed charging station specs

---
+++ database/migrations/2026_03_10_122321_create_charging_stations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('charging_stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->string('connector_type');
+            $table->boolean('is_available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('charging_stations');
+    }
+};
