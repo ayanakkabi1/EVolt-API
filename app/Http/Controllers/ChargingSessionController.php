6c81066b50fafa8c64c8ff82118d6b559d2e2a01
ayanakkabi1 <aynour200609@gmail.com>
2026-03-10 14:45:59 +0000

    feat: Implement ChargingSessionController with resource methods

---
+++ app/Http/Controllers/ChargingSessionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChargingSession;
+use Illuminate\Http\Request;
+
+class ChargingSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChargingSession $chargingSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChargingSession $chargingSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ChargingSession $chargingSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChargingSession $chargingSession)
+    {
+        //
+    }
+}
