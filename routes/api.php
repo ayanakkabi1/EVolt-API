3d306388f3ae4bcaae86fca4caefa4aaf2d6c57d
ayanakkabi1 <aynour200609@gmail.com>
2026-03-11 13:53:27 +0000

    chore(routes): add Reservation controller import in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controllers\Reservation;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
