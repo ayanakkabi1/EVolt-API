dcbfa52b9fbff6de32e34f7b9d524f70adcc791f
ayanakkabi1 <aynour200609@gmail.com>
2026-03-11 14:57:30 +0000

    fix(routes): correct import statement for ReservationController and adjust route definitions

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChargingStationController;
 use App\Http\Controllers\UserAuthController;
-use App\Http\Controllers\Reservation;
+use App\Http\Controllers\ReservationController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -11,4 +12,8 @@ Route::get('/user', function (Request $request) {
 Route::post('register',[UserAuthController::class,'register']);
 Route::post('login',[UserAuthController::class,'login']);
 Route::post('logout',[UserAuthController::class,'logout'])
+  ->middleware('auth:sanctum');
+Route::post('create',[ReservationController::class,'store'])
+  ->middleware('auth:sanctum');
+Route::post('create-charging-stations', [ChargingStationController::class, 'store'])
   ->middleware('auth:sanctum');
